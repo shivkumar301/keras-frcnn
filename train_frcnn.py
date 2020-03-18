c4353ab0cc06af267c7ef58723c27325be82f723
shivkumar301 <shivkumar301@users.noreply.github.com>
2020-03-18 11:07:47 +0530

    Update train_frcnn.py
    Changing:
    from keras import backend as K
    from keras.optimizers import Adam, SGD, RMSprop
    from keras.layers import Input
    from keras.models import Model
    from keras_frcnn import config, data_generators
    from keras_frcnn import losses as losses
    import keras_frcnn.roi_helpers as roi_helpers
    from keras.utils import generic_utils
    
    To:
    from tensorflow.keras import backend as K
    from tensorflow.keras.optimizers import Adam, SGD, RMSprop
    from tensorflow.keras.layers import Input
    from tensorflow.keras.models import Model
    from tensorflow.keras_frcnn import config, data_generators
    from tensorflow.keras_frcnn import losses as losses
    import tensorflow.keras_frcnn.roi_helpers as roi_helpers
    from tensorflow.keras.utils import generic_utils

--- train_frcnn.py
+++ train_frcnn.py
@@ -8,14 +8,22 @@ from optparse import OptionParser
 import pickle
 
 # from keras import backend as K
+# from keras.optimizers import Adam, SGD, RMSprop
+# from keras.layers import Input
+# from keras.models import Model
+# from keras_frcnn import config, data_generators
+# from keras_frcnn import losses as losses
+# import keras_frcnn.roi_helpers as roi_helpers
+# from keras.utils import generic_utils
+
 from tensorflow.keras import backend as K
-from keras.optimizers import Adam, SGD, RMSprop
-from keras.layers import Input
-from keras.models import Model
-from keras_frcnn import config, data_generators
-from keras_frcnn import losses as losses
-import keras_frcnn.roi_helpers as roi_helpers
-from keras.utils import generic_utils
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras_frcnn import config, data_generators
+from tensorflow.keras_frcnn import losses as losses
+import tensorflow.keras_frcnn.roi_helpers as roi_helpers
+from tensorflow.keras.utils import generic_utils
 
 sys.setrecursionlimit(40000)
 
